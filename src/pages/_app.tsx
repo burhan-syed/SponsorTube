19ca0f85dba748e4d90c720e74f50fc2aa45221f
Burhan Syed <burhanudinsyed@gmail.com>
2022-12-06 18:15:36 -0600

    tooltip provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,7 +5,9 @@ import { CookiesProvider } from "react-cookie";
 import type { Session } from "next-auth";
 import type { AppType } from "next/app";
 import { trpc } from "../utils/trpc";
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+import { TooltipProvider } from "@radix-ui/react-tooltip";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -14,8 +16,10 @@ const MyApp: AppType<{ session: Session | null }> = ({
   return (
     <CookiesProvider>
       <SessionProvider session={session}>
-        <Component {...pageProps} />
-        <ReactQueryDevtools initialIsOpen={false} />
+        <TooltipProvider>
+          <Component {...pageProps} />
+          <ReactQueryDevtools initialIsOpen={false} />
+        </TooltipProvider>
       </SessionProvider>
     </CookiesProvider>
   );
