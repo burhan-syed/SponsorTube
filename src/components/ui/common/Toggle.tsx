98f656b7584b232e206215e804ed45a66dd091b5
Burhan Syed <burhanudinsyed@gmail.com>
2023-01-16 16:18:45 -0500

    toggle style

--- src/components/ui/common/Toggle.tsx
+++ src/components/ui/common/Toggle.tsx
@@ -1,5 +1,7 @@
 import * as TogglePrimitive from "@radix-ui/react-toggle";
 import React from "react";
+import TouchResponse from "./TouchResponse";
+import useIsPressed from "@/hooks/useIsPressed";
 
 interface ToggleProps {
   children: React.ReactNode;
@@ -7,14 +9,22 @@ interface ToggleProps {
   onPressedChange(p: boolean): void;
 }
 
-const Toggle = ({ children, pressed, onPressedChange }: ToggleProps) => (
-  <TogglePrimitive.Root
-    pressed={pressed}
-    onPressedChange={onPressedChange}
-    className={"flex h-6 w-6 items-center justify-center rounded-md data-[state=on]:bg-blue-400 bg-white"}
-  >
-    {children}
-  </TogglePrimitive.Root>
-);
+const Toggle = ({ children, pressed, onPressedChange }: ToggleProps) => {
+  const { containerRef, isPressed } = useIsPressed();
+  return (
+    <div ref={containerRef} className="relative">
+      <TogglePrimitive.Root
+        pressed={pressed}
+        onPressedChange={onPressedChange}
+        className={
+          "relative z-10 flex h-9 w-9 items-center  justify-center rounded-full border border-transparent bg-th-chipBackground transition-all ease-in-out hover:bg-th-chipBackgroundHover data-[state=on]:border-th-chipBackground data-[state=on]:bg-th-textPrimary data-[state=on]:text-th-textPrimaryInverse    "
+        }
+      >
+        {children}
+      </TogglePrimitive.Root>
+      <TouchResponse isPressed={isPressed} className="rounded-full" />
+    </div>
+  );
+};
 
 export default Toggle;
