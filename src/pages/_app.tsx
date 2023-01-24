cf1c82d40a9da4aae0a6b66d661c757f0d3278ba
Burhan Syed <burhanudinsyed@gmail.com>
2023-01-24 09:13:37 -0600

    change query defaults

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,15 +6,29 @@ import type { Session } from "next-auth";
 import type { AppType } from "next/app";
 import { trpc } from "../utils/trpc";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { useQueryClient } from '@tanstack/react-query'
 
 import { TooltipProvider } from "@radix-ui/react-tooltip";
 import Head from "next/head";
 import SessionRequiredDialogueWrapper from "@/components/ui/dialogue/SessionRequiredDialogueWrapper";
 
+
+
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
+  const queryClient = useQueryClient({})
+  queryClient.setDefaultOptions({
+    queries: {
+      staleTime: Infinity,
+      refetchOnWindowFocus: false,
+      refetchOnMount: false,
+      refetchOnReconnect: false,
+
+    }
+  })
+
   return (
     <>
       <Head>
