839889da04b8949d8c823bdd1ee0ae5471583f48
Burhan Syed <burhanudinsyed@gmail.com>
2023-01-27 13:37:49 -0600

    sticky video embed

--- src/pages/video.tsx
+++ src/pages/video.tsx
@@ -6,7 +6,7 @@ import VideoInfo from "../components/ui/VideoInfo";
 import VideoEmbed from "../components/ui/VideoEmbed";
 import SponsorTranscripts from "../components/transcripts/SponsorTranscripts";
 import type { NextPage } from "next";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import VideoPageLoader from "@/components/ui/loaders/VideoPageLoader";
 import GridVideoView from "@/components/ui/GridVideoView";
 import GridVideoLoader from "@/components/ui/loaders/GridVideoLoader";
@@ -31,6 +31,20 @@ const Home: NextPage = ({}) => {
   const seekTo = (start: number, end: number) => {
     setVideoSeek((p) => [(p?.[0] ?? 0) + 1, start, end]);
   };
+
+  const [horizontal, setHorizonal] = useState<boolean>();
+  useEffect(() => {
+    setHorizonal(window.innerWidth >= 1024);
+    const onWindowResize = () => {
+      setHorizonal(window.innerWidth >= 1024);
+    };
+    window.addEventListener("resize", onWindowResize);
+
+    return () => {
+      window.removeEventListener("resize", onWindowResize);
+    };
+  }, []);
+
   return (
     <>
       <Head>
@@ -39,7 +53,7 @@ const Home: NextPage = ({}) => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
-      <div className="p-4">
+      <div className="relative sm:p-4">
         <div className="flex flex-col gap-2 lg:flex-row">
           {videoInfo.isLoading ? (
             <>
@@ -47,45 +61,64 @@ const Home: NextPage = ({}) => {
             </>
           ) : videoInfo.data ? (
             <>
-              <div className="flex flex-none flex-col lg:w-1/3">
-                {videoInfo.data.basic_info.embed?.iframe_url && (
-                  <VideoEmbed
-                    className="w-full overflow-hidden rounded-lg outline-none"
-                    videoID={videoInfo.data.basic_info.id}
-                    width={videoInfo.data.basic_info.embed?.width}
-                    height={videoInfo.data.basic_info.embed?.height}
-                    videoSeek={videoSeek}
-                  />
-                )}
-                <VideoInfo
-                  videoId={videoID}
-                  title={videoInfo.data.basic_info.title}
-                  views={videoInfo.data.basic_info.view_count}
-                  likes={videoInfo.data.basic_info.like_count}
-                  description={videoInfo.data.basic_info.short_description}
-                  descriptionRuns={videoInfo.data.basic_info.description?.runs}
-                  uploadDate={videoInfo.data.basic_info.upload_date}
-                  channelName={videoInfo.data.basic_info.channel.name}
-                  channelID={videoInfo.data.basic_info.channel_id}
-                  channelSubscribers={
-                    videoInfo.data.basic_info.channel.subscriber_count
-                  }
-                  channelThumbnail={videoInfo.data.basic_info.channel.thumbnail}
-                  channelIsVerified={
-                    videoInfo.data.basic_info.channel.is_verified
-                  }
-                  channelIsVerifiedArtist={
-                    videoInfo.data.basic_info.channel.is_verified_artist
-                  }
-                  channelURL={videoInfo.data.basic_info.channel.url}
+              {horizontal === undefined && (
+                <div className="skeleton-box aspect-video w-full"></div>
+              )}
+              {horizontal === false && (
+                <VideoEmbed
+                  className="sticky top-0 z-50 w-full overflow-hidden outline-none sm:relative sm:rounded-lg"
+                  videoID={videoInfo.data.basic_info.id}
+                  width={videoInfo.data.basic_info.embed?.width}
+                  height={videoInfo.data.basic_info.embed?.height}
+                  videoSeek={videoSeek}
                 />
+              )}
+              <div className="flex flex-none flex-col px-4 sm:px-0 lg:w-1/3">
+                <div className="lg:sticky lg:top-5">
+                  {videoInfo.data.basic_info.embed?.iframe_url &&
+                    horizontal && (
+                      <VideoEmbed
+                        className="w-full overflow-hidden outline-none sm:rounded-lg"
+                        videoID={videoInfo.data.basic_info.id}
+                        width={videoInfo.data.basic_info.embed?.width}
+                        height={videoInfo.data.basic_info.embed?.height}
+                        videoSeek={videoSeek}
+                      />
+                    )}
+                  <VideoInfo
+                    videoId={videoID}
+                    title={videoInfo.data.basic_info.title}
+                    views={videoInfo.data.basic_info.view_count}
+                    likes={videoInfo.data.basic_info.like_count}
+                    description={videoInfo.data.basic_info.short_description}
+                    descriptionRuns={
+                      videoInfo.data.basic_info.description?.runs
+                    }
+                    uploadDate={videoInfo.data.basic_info.upload_date}
+                    channelName={videoInfo.data.basic_info.channel.name}
+                    channelID={videoInfo.data.basic_info.channel_id}
+                    channelSubscribers={
+                      videoInfo.data.basic_info.channel.subscriber_count
+                    }
+                    channelThumbnail={
+                      videoInfo.data.basic_info.channel.thumbnail
+                    }
+                    channelIsVerified={
+                      videoInfo.data.basic_info.channel.is_verified
+                    }
+                    channelIsVerifiedArtist={
+                      videoInfo.data.basic_info.channel.is_verified_artist
+                    }
+                    channelURL={videoInfo.data.basic_info.channel.url}
+                  />
+                </div>
               </div>
             </>
           ) : (
             (videoInfo.error || true) && "something went wrong"
           )}
 
-          <div className="flex w-full flex-col gap-2">
+          <div className="flex w-full flex-col gap-2 px-4 sm:px-0">
             {/* Display this outside of videoInfoLoader to grab sponsorsegments with videoID*/}
             <SponsorTranscripts
               videoID={videoID}
@@ -94,14 +127,17 @@ const Home: NextPage = ({}) => {
             />
           </div>
         </div>
-        {videoInfo.isLoading ? (
-          <GridVideoLoader />
-        ) : (
-          videoInfo.data?.watch_next &&
-          (videoInfo.data.watch_next?.length ?? 0) > 0 && (
-            <GridVideoView videos={videoInfo.data.watch_next} />
-          )
-        )}
+
+        <div className="px-4 sm:px-0 py-4">
+          {videoInfo.isLoading ? (
+            <GridVideoLoader />
+          ) : (
+            videoInfo.data?.watch_next &&
+            (videoInfo.data.watch_next?.length ?? 0) > 0 && (
+              <GridVideoView videos={videoInfo.data.watch_next} />
+            )
+          )}
+        </div>
       </div>
     </>
   );
