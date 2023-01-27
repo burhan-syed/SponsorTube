0148cc83b9bd4626710b9dd40ca585e6f555063b
Burhan Syed <burhanudinsyed@gmail.com>
2023-01-27 16:35:53 -0600

    fixed header

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -15,15 +15,18 @@ const Header = ({
   }, [searchInitialValue]);
 
   return (
-    <header className="flex h-12 w-screen items-center">
-      <Link href={"/"}>
-        <a>Home</a>
-      </Link>
-      <div className="h-11 w-full max-w-6xl px-2 md:w-2/3 md:px-0">
-        <Search initialValue={searchInitialText} />
-      </div>
-      <Auth />
-    </header>
+    <>
+      <header className="fixed top-0 z-50 flex h-12 w-screen items-center justify-between bg-th-baseBackground px-4 sm:h-14">
+        <Link href={"/"}>
+          <a>Home</a>
+        </Link>
+        <div className="h-10 w-full max-w-6xl px-2 md:w-2/3 md:px-0">
+          <Search initialValue={searchInitialText} />
+        </div>
+        <Auth />
+      </header>
+      <div className="h-12 w-screen sm:h-14"></div>
+    </>
   );
 };
 
