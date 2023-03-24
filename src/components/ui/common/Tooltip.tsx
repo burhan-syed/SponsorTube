8d874999ab260e6cf318ff3e475417602925d30e
Burhan Syed <burhanudinsyed@gmail.com>
2023-03-24 14:36:34 -0500

    touchable tooltip

--- src/components/ui/common/Tooltip.tsx
+++ src/components/ui/common/Tooltip.tsx
@@ -1,19 +1,52 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import * as TooltipPrimitive from "@radix-ui/react-tooltip";
 
 const ToolTip = ({
   children,
   text,
+  manualControl,
   tooltipOptions = { side: "bottom", sideOffset: 10 },
 }: {
   children: React.ReactNode;
-  text: string;
+  text: string | React.ReactNode;
+  manualControl?: boolean;
   tooltipOptions?: TooltipPrimitive.TooltipContentProps;
 }) => {
+  const [isOpen, setIsOpen] = useState<boolean>();
+  const [focused, setFocused] = useState(false); 
+  const triggerRef = useRef<HTMLSpanElement>(null);
+  useEffect(() => {
+    let timeout: any;
+    if (isOpen && manualControl && focused) {
+      timeout = setTimeout(() => {console.log("blur?", triggerRef?.current?.blur); setIsOpen(false); setFocused(false);  if(triggerRef.current){triggerRef?.current?.blur(); triggerRef.current.tabIndex = 1}}, 2000);
+    }
+
+    return () => {
+      timeout && clearTimeout(timeout);
+    };
+  }, [manualControl, isOpen, focused]);
+
   return (
-    <TooltipPrimitive.Root>
+    <TooltipPrimitive.Root open={manualControl ? isOpen : undefined}>
       <TooltipPrimitive.Trigger asChild>
-        <span tabIndex={0}>{children}</span>
+        <span
+          ref={triggerRef}
+          tabIndex={0}
+          onMouseEnter={() => {
+            manualControl && setIsOpen(true);
+          }}
+          onMouseLeave={() => {
+            manualControl && setIsOpen(false);
+          }}
+          onFocus={() => {
+            if(manualControl) {setIsOpen(true); setFocused(true);}
+          }}
+          onBlur={() => {
+            manualControl && setIsOpen(false);
+          }}
+        >
+          {children}
+        </span>
       </TooltipPrimitive.Trigger>
       <TooltipPrimitive.Portal>
         {text && (
