52d365b598fcbdf89e343ac6e1dba88a45683b56
Burhan Syed <burhanudinsyed@gmail.com>
2023-03-27 14:29:00 -0500

    sponsor pills wrapper button

--- src/components/ui/channel/ChannelStatsWrapper/ChannelSponsorsPills.tsx
+++ src/components/ui/channel/ChannelStatsWrapper/ChannelSponsorsPills.tsx
@@ -8,6 +8,8 @@ import clsx from "clsx";
 
 const ChannelSponsorsPills = ({ sponsors }: { sponsors: string[] }) => {
   const { containerRef, isPressed } = useIsPressed();
+  const { containerRef: containerRef2, isPressed: isPressed2 } = useIsPressed();
+
   const [showExpand, setShowExpand] = useState<boolean>();
   const [expand, setExpand] = useState(false);
   const [scrolled, setScrolled] = useState(false);
@@ -59,70 +61,99 @@ const ChannelSponsorsPills = ({ sponsors }: { sponsors: string[] }) => {
   return (
     <div
       className={clsx(
-        "relative rounded-3xl ",
-        expand && showExpand && "ring-1 ring-th-chipBackground"
+        "rounded-3xl ring-1 ring-th-chipBackground ",
+        expand && showExpand && ""
       )}
     >
-      <ul
-        ref={listRef}
+      <button
+        ref={containerRef2}
+        disabled={!showExpand}
         className={clsx(
-          "flex flex-row flex-wrap items-center justify-center gap-x-2 gap-y-1 py-2  ",
-          "transition-transform  ease-in-out before:pointer-events-none  before:absolute before:top-[100px] before:z-20 before:h-[100px] before:w-full before:bg-gradient-to-t  before:from-th-baseBackground before:via-transparent  before:to-transparent after:pointer-events-none  after:absolute after:top-0 after:z-10 after:h-[100px] after:w-full after:bg-gradient-to-b after:from-th-baseBackground after:via-transparent after:to-transparent after:transition-opacity after:ease-in",
-          expand ? "" : "max-h-[200px] overflow-y-scroll overscroll-contain",
-          expand || atBottom || !showExpand
-            ? `before:opacity-0 ${
-                atBottom ? " before:transition-opacity before:ease-in" : ""
-              }`
-            : ` before:opacity-100 before:transition-opacity before:ease-in  `,
-          scrolled && !expand ? "after:opacity-100" : "after:opacity-0"
+          "relative flex h-10 w-full items-center justify-center rounded-t-3xl bg-th-chipBackground ",
+          showExpand && "hover:bg-th-chipBackgroundHover"
         )}
+        onClick={() =>
+          showExpand &&
+          setExpand((e) => {
+            if (e) {
+              window.scrollTo({ top: 0, behavior: "smooth" });
+            }
+            return !e;
+          })
+        }
       >
-        {sponsors.map((s) => (
-          <li key={s}>
-            <Button
-              onClick={() => console.log(s)}
-              variant={"primary"}
-              size={"small"}
-              className=""
-            >
-              <div className={"z-10 flex items-center"}>
-                <span className="">{s}</span>
-              </div>
+        <h2 className="text-base">Identified Sponsors</h2>
+        <TouchResponse
+          variant="ring"
+          className={"rounded-t-3xl"}
+          isPressed={isPressed2 && !!showExpand}
+        />
+      </button>
 
-              <div
-                className="absolute h-full w-full rounded-full backdrop-blur-md"
-                style={{ backgroundColor: `${TAGS.get("BRAND")}90` }}
-              ></div>
-            </Button>
-          </li>
-        ))}
-      </ul>
-      {showExpand && (
-        <button
-          ref={containerRef}
-          className="relative flex h-10 w-full items-center justify-center rounded-b-3xl bg-th-chipBackground hover:bg-th-chipBackgroundHover  "
-          onClick={() =>
-            setExpand((e) => {
-              if (e) {
-                window.scrollTo({ top: 0, behavior: "smooth" });
-              }
-              return !e;
-            })
-          }
+      <div className={"relative"}>
+        <ul
+          ref={listRef}
+          className={clsx(
+            "flex flex-row flex-wrap items-center justify-center gap-x-2 gap-y-1 py-2 transition-all  duration-200 ease-in-out md:px-1 ",
+            " before:pointer-events-none  before:absolute before:top-[100px] before:z-20 before:h-[100px] before:w-full before:bg-gradient-to-t  before:from-th-baseBackground before:via-transparent  before:to-transparent after:pointer-events-none  after:absolute after:top-0 after:z-10 after:h-[100px] after:w-full after:bg-gradient-to-b after:from-th-baseBackground after:via-transparent after:to-transparent after:transition-opacity after:ease-in",
+            expand
+              ? "max-h-[100vh] overflow-y-auto"
+              : "max-h-[200px] overflow-y-scroll",
+            expand || atBottom || !showExpand
+              ? `before:opacity-0 ${
+                  atBottom ? " before:transition-opacity before:ease-in" : ""
+                }`
+              : ` before:opacity-100 before:transition-opacity before:ease-in  `,
+            scrolled && !expand ? "after:opacity-100" : "after:opacity-0"
+          )}
         >
-          <BiChevronDown
-            className={clsx(
-              expand ? "rotate-180" : "",
-              "h-6 w-6 flex-none transition-transform ease-in-out"
-            )}
-          />
-          <TouchResponse
-            variant="ring"
-            className={"rounded-b-3xl"}
-            isPressed={isPressed}
-          />
-        </button>
-      )}
+          {sponsors.map((s) => (
+            <li key={s}>
+              <Button
+                onClick={() => console.log(s)}
+                variant={"primary"}
+                size={"small"}
+                className=""
+              >
+                <div className={"z-10 flex items-center"}>
+                  <span className="">{s}</span>
+                </div>
+
+                <div
+                  className="absolute h-full w-full rounded-full backdrop-blur-md"
+                  style={{ backgroundColor: `${TAGS.get("BRAND")}90` }}
+                ></div>
+              </Button>
+            </li>
+          ))}
+        </ul>
+        {showExpand && (
+          <button
+            ref={containerRef}
+            className="relative flex h-10 w-full items-center justify-center rounded-b-3xl bg-th-chipBackground hover:bg-th-chipBackgroundHover  "
+            onClick={() =>
+              setExpand((e) => {
+                if (e) {
+                  window.scrollTo({ top: 0, behavior: "smooth" });
+                }
+                return !e;
+              })
+            }
+          >
+            <BiChevronDown
+              className={clsx(
+                expand ? "rotate-180" : "",
+                "h-6 w-6 flex-none transition-transform ease-in-out"
+              )}
+            />
+            <TouchResponse
+              variant="ring"
+              className={"rounded-b-3xl"}
+              isPressed={isPressed}
+            />
+          </button>
+        )}
+      </div>
     </div>
   );
 };
