05628c30edb528b99f344410701c65c90ac0765a
Burhan Syed <burhanudinsyed@gmail.com>
2023-03-29 14:12:26 -0500

    refactor backfill script

--- scripts/backfillLockedSponsorTimes.ts
+++ scripts/backfillLockedSponsorTimes.ts
@@ -3,88 +3,28 @@ import csv from "csv-parser";
 import fs from "fs";
 import path from "path";
 
-import type { SponsorTimes, SponsorCategories } from "@prisma/client";
+import type { SponsorTimes } from "@prisma/client";
 
-type ImportSponsorTime = {
-  videoID: string;
-  startTime: string;
-  endTime: string;
-  votes: string;
-  locked: string;
-  incorrectVotes: string;
-  UUID: string;
-  userID: string;
-  timeSubmitted: string;
-  views: string;
-  category: string;
-  actionType: string;
-  service: string;
-  videoDuration: string;
-  hidden: string;
-  reputation: string;
-  shadowHidden: string;
-  hashedVideoID: string;
-  userAgent: string;
-  description: string;
-};
+//last ran on 03/29/2023
+const PROCESS_TO_DATE = new Date("01/01/2022");
 
 const prisma = new PrismaClient({ log: ["info", "warn"] });
 const results = [] as SponsorTimes[][]; //new Map<number,SponsorTimes[]>();
-let totalCount = 0;
+let totalRowsCount = 0;
 let totalCreated = 0;
 let adjustedCount = 0;
-const ROWSPERGROUP = 10000;
+const ROWSPERGROUP = 1000;
 const ROWSTARTNO = 0;
-const parseRow = async (row: ImportSponsorTime) => {
-  totalCount += 1;
-
-  if (
-    totalCount >= ROWSTARTNO &&
-    row.category === "sponsor" &&
-    row.locked === "1"
-  ) {
-    adjustedCount += 1;
-
-    const parsed = {
-      UUID: row.UUID,
-      category: row.category as unknown as SponsorCategories,
-      videoID: row.videoID,
-      startTime: parseFloat(row.startTime),
-      endTime: parseFloat(row.endTime),
-      votes: parseInt(row.votes),
-      locked: row.locked === "1" ? true : false,
-      incorrectVotes: parseInt(row.incorrectVotes),
-      userID: row.userID,
-      timeSubmitted: new Date(parseInt(row.timeSubmitted)),
-      views: parseInt(row.views),
-      actionType: row.actionType,
-      service: row.service,
-      videoDuration: parseFloat(row.videoDuration),
-      hidden: row.hidden === "1" ? true : false,
-      reputation: parseFloat(row.reputation),
-      shadowHidden: row.shadowHidden === "1" ? true : false,
-      hashedVideoID: row.hashedVideoID,
-      userAgent: row.userAgent,
-      description: row.description,
-    };
-    const groupNo = Math.floor(adjustedCount / (ROWSPERGROUP + 1));
-    if (results && results?.[groupNo] && Array.isArray(results[groupNo])) {
-      results[groupNo]?.push(parsed);
-    } else {
-      results.push([parsed]);
-    }
-    // console.log(
-    //   `pushed ${adjustedCount} (${groupNo}:${results[groupNo]?.length}): `,
-    //   parsed.UUID
-    // );
-
-    if (results?.[groupNo]?.length === ROWSPERGROUP) {
-      insertIntoDB(groupNo);
-    }
+const parseRow = async (row: SponsorTimes) => {
+  adjustedCount += 1;
+  const groupNo = Math.floor(adjustedCount / (ROWSPERGROUP + 1));
+  if (results && results?.[groupNo] && Array.isArray(results[groupNo])) {
+    results[groupNo]?.push(row);
   } else {
-    if (totalCount < ROWSTARTNO) {
-      console.log("skip ", totalCount);
-    }
+    results.push([row]);
+  }
+  if (results?.[groupNo]?.length === ROWSPERGROUP) {
+    insertIntoDB(groupNo);
   }
 };
 
@@ -97,43 +37,123 @@ const insertIntoDB = async (groupNo: number) => {
     skipDuplicates: true,
   });
   totalCreated += create.count;
-  console.log("created ", create.count);
+  //totalCreated += results?.[groupNo]?.length ?? 0;
+  //console.log("created ", create.count);
   console.log("total created ", totalCreated);
 
-  //clear results array https://stackoverflow.com/questions/1232040/how-do-i-empty-an-array-in-javascript
-  while (results?.[groupNo] && (results?.[groupNo]?.length ?? 0) > 0) {
-    results[groupNo]?.pop();
+  //clear results array; best method discussed https://stackoverflow.com/questions/1232040/how-do-i-empty-an-array-in-javascript
+  if (results?.[groupNo] && results?.[groupNo]?.length) {
+    results[groupNo] = [];
   }
+  console.log(`cleared? (${groupNo}:${results[groupNo]?.length})`);
 };
 
 const main = async () => {
-  fs.createReadStream(path.join(__dirname, "./data/sponsorTimes.csv"))
-    .pipe(csv())
-    .on("data", async (data) => {
-      //console.log("row ", data?.["UUID"]);
-      if (data?.["UUID"]) {
-        await parseRow(data);
-      }
-    })
-    .on("end", async () => {
-      console.log("Finished parsing csv");
-      console.log("Total rows: " + totalCount);
-      console.log("Adjusted count:" + adjustedCount);
-      console.log("Writing last rows");
+  console.log("PROCESSING TO ", PROCESS_TO_DATE);
 
-      await Promise.all(
-        results.map(async (group, i) => {
-          if (group.length > 0) {
-            await insertIntoDB(i);
+  const readStream = fs
+    .createReadStream(path.join(__dirname, "./data/sponsorTimes.csv"))
+    .pipe(
+      csv({
+        headers: [
+          "videoID",
+          "startTime",
+          "endTime",
+          "votes",
+          "locked",
+          "incorrectVotes",
+          "UUID",
+          "userID",
+          "timeSubmitted",
+          "views",
+          "category",
+          "actionType",
+          "service",
+          "videoDuration",
+          "hidden",
+          "reputation",
+          "shadowHidden",
+          "hashedVideoID",
+          "userAgent",
+          "description",
+        ],
+        skipLines: 0,
+        mapValues: ({ header, index, value }) => {
+          switch (header) {
+            case "startTime":
+            case "endTime":
+            case "reputation":
+            case "videoDuration":
+              return parseFloat(value);
+            case "votes":
+            case "incorrectVotes":
+            case "views":
+              return parseInt(value);
+            case "locked":
+            case "hidden":
+            case "shadowHidden":
+              return value === "1" ? true : false;
+            case "timeSubmitted":
+              return new Date(parseInt(value));
+            default:
+              return value;
           }
-        })
-      );
+        },
+      })
+    );
 
-      console.log("Total Created:" + totalCreated);
-    });
+  readStream.on("data", async (row: SponsorTimes) => {
+    totalRowsCount++;
+    if (
+      row?.["UUID"] &&
+      row?.["category"] === "sponsor" &&
+      row?.locked &&
+      row.timeSubmitted &&
+      row.timeSubmitted >= PROCESS_TO_DATE
+    ) {
+      // console.log(
+      //   ">>>>found",
+      //   row?.UUID,
+      //   row?.timeSubmitted,
+      //   row.timeSubmitted > PROCESS_TO_DATE
+      // );
+      parseRow(row);
+    } else if (row.timeSubmitted) {
+      // console.log(
+      //   "skip",
+      //   row.timeSubmitted,
+      //   PROCESS_TO_DATE,
+      //   row.timeSubmitted > PROCESS_TO_DATE
+      // );
+    } else {
+      console.log("NO TIME SUBMITTED");
+    }
+
+    // if (totalRowsCount >= 10000000) {
+    //   readStream.destroy();
+    // }
+  });
+
+  readStream.on("end", async () => {
+    console.log("Finished parsing csv");
+    console.log("Total rows: " + totalRowsCount);
+    console.log("Total Created:" + totalCreated);
+    console.log("Writing last rows");
+
+    await Promise.allSettled(
+      results.map(async (group, i) => {
+        if (group.length > 0) {
+          await insertIntoDB(i);
+        }
+      })
+    );
+
+    //console.log("Total Created:" + totalCreated);
+  });
 };
 
 main().catch((e) => {
   console.error(e);
-  console.log("total Rows: ", totalCount);
+  console.log("total Rows: ", totalRowsCount);
+  console.log("total Created", totalCreated);
 });
