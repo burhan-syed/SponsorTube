26944bba5b06d5383eb19d8d1c273701dada5d4b
Burhan Syed <burhanudinsyed@gmail.com>
2023-04-18 16:26:09 -0500

    recents page use video cards

--- src/pages/recent.tsx
+++ src/pages/recent.tsx
@@ -1,80 +1,74 @@
 import Header from "@/components/Header";
 import SegmentsPreview from "@/components/ui/SegmentsPreview";
 import { Button } from "@/components/ui/common/Button";
+import GridVideoLoader from "@/components/ui/loaders/GridVideoLoader";
+import GridVideoView from "@/components/video/GridVideoView";
+import { VideoCardInfo } from "@/types/schemas";
 import { api } from "@/utils/api";
 import clsx from "clsx";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
+const limit = 30;
 const RecentsPage = () => {
   const recentVods = api.video.getRecent.useInfiniteQuery(
-    { limit: 20, withSponsors: true },
+    { limit: limit, withSponsors: true },
     { getNextPageParam: (lastPage) => lastPage.nextCursor }
   );
+  const flatVideos = recentVods.data?.pages
+    .map((p) =>
+      p.vods.map((v) => ({
+        id: v.id,
+        title: v.title,
+        publishedDate: v.published,
+        thumbnail: v.thumbnail
+          ? {
+              url: v.thumbnail,
+              height: v?.thumbnailHeight ?? undefined,
+              width: v?.thumbnailWidth ?? undefined,
+            }
+          : undefined,
+        author: {
+          id: v.channelId,
+          name: v.Channel.name ?? "",
+          thumbnail: v.Channel.thumbnail
+            ? { url: v.Channel.thumbnail }
+            : undefined,
+        },
+      }))
+    )
+    .flat() as VideoCardInfo[];
   return (
     <div>
-      <Header/>
-      {recentVods.isLoading ? (
-        "loading"
-      ) : recentVods.data?.pages ? (
-        <>
-          {recentVods.data.pages.map((p) => (
-            <div key={p.nextCursor}>
-              {p.vods.map((video) => (
-                <div key={video.id} className="flex gap-2 p-2">
-                  <Link href={`/video?v=${video.id}`}>
-                    <a
-                      className={
-                        "relative aspect-video h-20 flex-none overflow-hidden rounded-2xl bg-th-additiveBackground bg-opacity-5 "
-                      }
-                    >
-                      {video?.thumbnail ? (
-                        <Image
-                          src={video?.thumbnail}
-                          alt=""
-                          width={video?.thumbnailWidth ?? undefined}
-                          height={video?.thumbnailHeight ?? undefined}
-                          layout="responsive"
-                          unoptimized={true}
-                        />
-                      ) : (
-                        <>no thumb</>
-                      )}
-                    </a>
-                  </Link>
-                  <div className="flex flex-col gap-1">
-                    <h2 className="font-semibold">{video.title}</h2>
-                    <Link href={`/channel/${video.Channel.id}`}>
-                      <a className="underline">{video.Channel.name}</a>
-                    </Link>
-                    <p>{video.id} : {video.published.toLocaleDateString()}</p>
-                    <div>
-                      <SegmentsPreview
-                        videoId={video.id}
-                        className="flex flex-wrap"
-                      />
-                    </div>
-                  </div>
-                </div>
-              ))}
-            </div>
-          ))}
-        </>
-      ) : (
-        "something went wrong"
-      )}
-      <Button
-        disabled={
-          recentVods.isLoading ||
-          recentVods.isFetching ||
-          !recentVods.hasNextPage
-        }
-        loading={recentVods.isLoading || recentVods.isFetching}
-        onClick={() => recentVods.fetchNextPage()}
-      >
-        load more
-      </Button>
+      <Header />
+      <section className="mx-auto my-2 px-4 md:px-[calc(10vw)] lg:my-4 2xl:max-w-[192rem]">
+        {recentVods.isLoading ? (
+          <GridVideoLoader count={limit} />
+        ) : flatVideos?.length ?? 0 > 0 ? (
+          <>
+            <GridVideoView
+              videos={flatVideos}
+              showLoading={recentVods.isFetchingNextPage ? limit : 0}
+            />
+          </>
+        ) : (
+          "something went wrong"
+        )}
+        <div className="my-2 flex w-full items-center justify-center lg:my-4">
+          <Button
+            disabled={
+              recentVods.isLoading ||
+              recentVods.isFetching ||
+              !recentVods.hasNextPage
+            }
+            loading={recentVods.isLoading || recentVods.isFetching}
+            onClick={() => recentVods.fetchNextPage()}
+          >
+            load more
+          </Button>
+        </div>
+      </section>
     </div>
   );
 };
