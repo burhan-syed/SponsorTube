567d425258d3e888296f13fdd4fc0865e8cd24ff
Burhan Syed <burhanudinsyed@gmail.com>
2023-05-01 16:09:29 -0500

    update toggle

--- src/components/ui/common/Toggle.tsx
+++ src/components/ui/common/Toggle.tsx
@@ -2,40 +2,51 @@ import * as TogglePrimitive from "@radix-ui/react-toggle";
 import React from "react";
 import TouchResponse from "./TouchResponse";
 import useIsPressed from "@/hooks/useIsPressed";
-import clsx from "clsx";
+import { VariantProps, cva } from "class-variance-authority";
+import { cn } from "@/utils/cn";
 
-type ToggleProps = {
-  className?: string;
-  children: React.ReactNode;
-  pressed: boolean;
-  onPressedChange(p: boolean): void;
-} & TogglePrimitive.ToggleProps &
-  React.RefAttributes<HTMLButtonElement>;
+const toggleVariants = cva(
+  "inline-flex items-center justify-center rounded-full transition-colors data-[state=on]:bg-th-textPrimary data-[state=on]:text-th-textPrimaryInverse focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-th-callToAction focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 ring-offset-background  hover:shadow hover:bg-th-chipBackgroundHover ",
+  {
+    variants: {
+      variant: {
+        default: "bg-th-chipBackground",
+        outline:
+          "bg-th-chipBackground border border-transparent hover:border-th-chipBackground data-[state=on]:border-th-chipBackground ",
+      },
+      size: {
+        default: "h-9 w-9",
+        sm: "h-9 px-2.5",
+        lg: "h-11 px-5",
+      },
+    },
+    defaultVariants: {
+      variant: "outline",
+      size: "default",
+    },
+  }
+);
 
-const Toggle = ({
-  className,
-  children,
-  pressed,
-  onPressedChange,
-  ...props
-}: ToggleProps) => {
+const Toggle = React.forwardRef<
+  React.ElementRef<typeof TogglePrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof TogglePrimitive.Root> &
+    VariantProps<typeof toggleVariants>
+>(({ className, variant, size, ...props }, ref) => {
   const { containerRef, isPressed } = useIsPressed();
+
   return (
     <div ref={containerRef} className="relative">
       <TogglePrimitive.Root
+        ref={ref}
+        className={cn(toggleVariants({ variant, size, className }))}
         {...props}
-        pressed={pressed}
-        onPressedChange={onPressedChange}
-        className={clsx(
-          className,
-          "relative z-10 flex h-9 w-9 items-center justify-center  rounded-full border border-transparent bg-th-chipBackground transition-all ease-in-out hover:border-th-chipBackground hover:bg-th-chipBackgroundHover hover:shadow data-[state=on]:border-th-chipBackground data-[state=on]:bg-th-textPrimary data-[state=on]:text-th-textPrimaryInverse"
-        )}
-      >
-        {children}
-      </TogglePrimitive.Root>
+      />
       <TouchResponse isPressed={isPressed} className="rounded-full" />
     </div>
   );
-};
+});
 
+Toggle.displayName = TogglePrimitive.Root.displayName;
+
+// export { Toggle, toggleVariants };
 export default Toggle;
