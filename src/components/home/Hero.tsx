b6b75af3f0328416fbf9ed734b970a6aebdfa562
Burhan Syed <burhanudinsyed@gmail.com>
2023-05-16 15:54:54 -0500

    hero text css

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -2,16 +2,16 @@ import React, { useEffect, useRef } from "react";
 import SlideUpText from "../ui/SlideUpText";
 import HomeSearch from "../search/HomeSearch";
 const Hero = () => {
-  const HeroText = ["Uncover the", "Brands Behind", "Video Sponsors"];
+  const HeroText = ["Uncover the", "brands behind", "video sponsors."];
   const HeroTextTransitionDuration = 500;
-  const HeroTextDelays = [200, 300, 400, 700, 800, 900];
+  const HeroTextDelays = [200, 300, 400, 800, 850, 900];
   const SecondaryTextInitialDelay =
     (HeroTextDelays[HeroTextDelays.length - 1] ?? 0) +
     HeroTextTransitionDuration;
   const SecondaryTextDelays = [5];
   const SecondayTextDuration = [200];
   const SubHeroText = [
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    "SponsorTube empowers you to effortlessly analyze the latest brands sponsoring videos and channels on YouTube. Lookup any channel or video to get started.",
   ];
 
   const searchContainerRef = useRef<HTMLDivElement>(null);
@@ -33,7 +33,7 @@ const Hero = () => {
   return (
     <div className="flex w-full flex-grow flex-col sm:gap-y-[4vh] sm:pt-[10vh] md:pt-[8vh] lg:pt-[10vh] xl:pt-[12vh] 2xl:pt-[20vh]">
       <h1
-        className="text-[calc(min(18vw,10rem))] font-semibold leading-[1] text-th-textPrimaryInverse sm:text-[10.6vw] md:text-[8vw] lg:text-[7.6vw] xl:text-[6.6vw] 2xl:text-[calc(min(5.5vw,12rem))]"
+        className="text-h1 font-semibold text-th-textPrimaryInverse"
         style={{ textShadow: "1px 2px 2px #00000020" }}
       >
         <SlideUpText
@@ -45,7 +45,7 @@ const Hero = () => {
       </h1>
       <div className="mt-[8vw] flex flex-col gap-y-[3vh] sm:mt-0 sm:gap-y-[4vh] ">
         <h2
-          className="px-1 text-[calc(min(4vw,2rem))] font-normal leading-[1.2] text-th-textPrimaryInverse sm:order-2 sm:text-[2.6vw] md:max-w-[60vw] md:text-[2vw] xl:text-[1.4vw] 2xl:text-[calc(min(1.5vw,2rem))]"
+          className="px-1 text-p font-normal leading-[1.2] text-th-textPrimaryInverse sm:order-2 md:max-w-[50vw] "
           style={{ textShadow: "1px 2px 2px #00000020" }}
         >
           <SlideUpText
