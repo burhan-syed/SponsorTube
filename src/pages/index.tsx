cfc2faee2e9c0e47a2eb2c7620e9f741e0cbb203
Burhan Syed <burhanudinsyed@gmail.com>
2023-05-16 15:58:22 -0500

    ssr cache recent vods

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,25 @@
 import { useEffect, useRef, useState } from "react";
 import Head from "next/head";
-import type { NextPage } from "next";
+import type {
+  GetServerSidePropsContext,
+  NextPage,
+} from "next";
+import { createServerSideHelpers } from "@trpc/react-query/server";
+import { prisma } from "@/server/db";
+import SuperJSON from "superjson";
+
 import HomeNavBar from "@/components/home/HomeNavBar";
 import Hero from "@/components/home/Hero";
 import HeroBG from "@/components/home/HeroBG";
 import HomeSectionsContainer from "@/components/home/HomeSectionsContainer";
 import ScrollComponent from "@/components/ui/ScrollComponent";
+import HomeRecentVods from "@/components/home/HomeRecentVods";
+import { appRouter } from "@/server/api/router";
+import { cn } from "@/utils/cn";
+import { FaChevronRight } from "react-icons/fa";
+import Link from "next/link";
+
+const RecentVodsLimit = 5;
 
 const Home: NextPage = () => {
   return (
@@ -20,8 +34,7 @@ const Home: NextPage = () => {
       </div>
       <div className="relative">
         <HeroBG />
-
-        <section className="mx-auto w-full px-4 pt-16 sm:items-center md:px-[calc(5vw)] 2xl:max-w-[192rem]">
+        <section className="mx-auto w-full px-4 pt-16 sm:items-center md:px-[5vw] 2xl:max-w-[192rem]">
           <div className="relative flex md:mx-[5vw] ">
             <Hero />
           </div>
@@ -30,26 +43,86 @@ const Home: NextPage = () => {
 
       <div className="mt-[10vw]">
         <HomeSectionsContainer>
-          <section className="mx-auto w-full p-[5vw] sm:items-center md:px-[calc(5vw)] 2xl:max-w-[192rem]">
-            <div className="md:px-[5vw]">
-              <section className="flex w-full flex-col items-start gap-y-[5vw] md:flex-row md:justify-between md:gap-x-[5vw] md:gap-y-0">
-                <h2 className="text-h2">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.{" "}
-                </h2>
-                <p className="text-p">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.{" "}
-                </p>
-              </section>
-              <ScrollComponent completeAt={0}>
-                <h2 className="text-[10vw] font-bold text-th-textSecondary/20 inline">scrolling text</h2>
-              </ScrollComponent>
-              <p className="h-[300vh]">?</p>
-            </div>
-          </section>
+          <>
+            <section className="mx-auto w-full p-[5vw] sm:items-center  2xl:max-w-[192rem]">
+              <div className="md:px-[5vw]">
+                <section className="flex w-full flex-col items-start gap-y-[5vw] md:flex-row md:justify-between md:gap-x-[5vw] md:gap-y-0">
+                  <h2 className="text-h2">
+                    We scan thousands of videos daily to automatically extract
+                    sponsor information.
+                  </h2>
+                  <p className="text-p">
+                    Brands, products, offers, and links are identified from
+                    sponsored ad reads. Segments are community voted for
+                    validity and corrected manually if necessary.
+                  </p>
+                </section>
+              </div>
+            </section>
+            <section className="relative flex flex-col items-center">
+              <div className="absolute top-0 mx-auto w-full max-w-full overflow-clip md:items-center md:px-[5vw] 2xl:max-w-[192rem] ">
+                <div className="w-full lg:w-3/4  xl:w-2/3">
+                  <ScrollComponent completeAt={0}>
+                    <h2 className="text-h1 inline px-[5vw] font-bold text-th-textPrimary/10 ">
+                      Latest Videos
+                    </h2>
+                  </ScrollComponent>
+                  <div>
+                    <Link
+                      href="/recent"
+                      className={
+                        "text-h2  group absolute right-[10vw] top-0 flex items-center gap-x-2 font-semibold text-th-textPrimary "
+                      }
+                    >
+                      <span className="translate-x-0 text-p transition-transform ease-in-out group-hover:-translate-x-2 duration-500">
+                        View All
+                      </span>
+                      <div className="aspect-square h-full rounded-lg lg:rounded-2xl bg-th-textPrimary">
+                        <FaChevronRight className=" p-1 lg:p-2 text-th-textPrimaryInverse" />
+                      </div>
+                    </Link>
+                  </div>
+                </div>
+              </div>
+              <div
+                className={cn(
+                  "mx-auto w-full px-[5vw] sm:items-center md:px-[calc(5vw)] 2xl:max-w-[192rem]",
+                  "my-[calc(min(12vw,8rem))] sm:my-[7.4vw] md:my-[5.6vw] lg:my-[5.4vw] xl:my-[4.8vw] 2xl:my-[calc(min(4.1vw,8.8rem))]"
+                )}
+              >
+                <div className="-mx-1 flex w-full flex-col sm:mx-0 md:px-[5vw]">
+                  <HomeRecentVods limit={RecentVodsLimit} />
+                </div>
+              </div>
+            </section>
+          </>
         </HomeSectionsContainer>
       </div>
     </>
   );
 };
+
+export async function getServerSideProps(
+  context: GetServerSidePropsContext<{}>
+) {
+  const helpers = createServerSideHelpers({
+    router: appRouter,
+    ctx: { prisma, session: null },
+    transformer: SuperJSON, // optional - adds superjson serialization
+  });
+  await helpers.video.getRecent.prefetchInfinite({
+    limit: RecentVodsLimit,
+    withSponsors: true,
+  });
+  context.res.setHeader(
+    'Cache-Control',
+    'public, s-maxage=3600, stale-while-revalidate=59'
+)
+  return {
+    props: {
+      trpcState: helpers.dehydrate(),
+    },
+  };
+}
 
 export default Home;
